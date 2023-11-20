9220faadeb47fde77947414d2a8eb0a4593f5639
aryawijayak <aryawijayak2004@gmail.com>
2023-11-20 20:00:18 +0700

    inisasi style

---
+++ lib/styles/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Warna {
+  static const white = Color(0xFFffffff);
+  static const black = Color(0xFF090D2B);
+  static const blue = Color(0xFF2A4CF1);
+  static const cyan = Color(0xFF3FBCFC);
+}
---
+++ lib/styles/fonts.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class Fonts {
+  static const TextStyle h1 = TextStyle(
+    fontFamily: 'Poppins',
+    color: Warna.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 30,
+  );
+}
