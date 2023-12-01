95a2d20525e02be943f541c264af55c340f2a975
syifakaffa <syifa.kaffa@ui.ac.id>
2023-12-01 17:07:03 +0700

    ubah main jadi splashscreen lagi

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         ),
         
         debugShowCheckedModeBanner: false,
-        home: Homescreen(),
+        home: SplashScreen(),
       ),
     );
   }
