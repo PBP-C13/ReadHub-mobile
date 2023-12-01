a8492ee14c14ffe0c2bf6833d07e209350fae857
aryawijayak <aryawijayak2004@gmail.com>
2023-12-01 09:32:20 +0700

    menu

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -12,19 +12,3 @@ class Warna {
   static const lightcyan = Color(0xFF1E395A);
   static const line = Color(0xFF35396F);
 }
-
-class Colorz {
-  static const white = Color(0xFFffffff);
-  static const black = Color(0xFF171314);
-  static const pink = Color(0xFFF3C3FB);
-  static const avogreen = Color(0xFFDEE287);
-  static const darkgreen = Color(0xFF24554C);
-  static const oceanblue = Color(0XFF3683EF);
-  static const skyblue = Color(0xFFACD8FA);
-  static const orange = Color(0xFFE65E2B);
-  static const lightbeige = Color(0xFFFDF5F1);
-  static const beige = Color(0xFFF3E6E0);
-  static const darkbeige = Color(0xFFE7D4C6);
-  static const ashbrown = Color(0xFFB0A198);
-  static const beigebg = Color(0xFFECDED6);
-}
