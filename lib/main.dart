485a8a08c9760ec9e7c3d703f35c483cc92f6341
syifakaffa <syifa.kaffa@ui.ac.id>
2023-12-03 22:38:15 +0700

    bikin button dan nyambungin ke detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
           useMaterial3: true,
         ),
-        home: Homescreen(),
+        home: SplashScreen(),
       ),
     );
   }
