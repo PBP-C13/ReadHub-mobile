2ecb4dc782c79906b942e199e63cd3f72df10c62
Aiza <aizaderisyana@yahoo.co.id>
2023-12-14 23:33:21 +0700

    update kecil detail

--- lib/screens/flow/detail_book.dart
+++ lib/screens/flow/detail_book.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -7,6 +8,7 @@ import 'package:readhub/screens/flow/borrow.dart';
 import 'package:readhub/styles/colors.dart';
 import 'dart:convert';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:readhub/widgets/favorit_form.dart';
 import 'package:readhub/widgets/navbar.dart';
 import 'package:readhub/screens/navigation/home.dart';
 import 'package:readhub/screens/navigation/explore.dart';
@@ -93,11 +95,9 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
-    bool isFavorite = false;
 
     return Scaffold(
       backgroundColor: Warna.background,
-      bottomNavigationBar: BottomNavBar(index: 0),
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.fromLTRB(27, 58, 0, 60),
@@ -112,32 +112,49 @@ class _DetailScreenState extends State<DetailScreen> {
 
 // <<< Tombol Arrow >>> //
 
-              Container(
-                margin: EdgeInsets.fromLTRB(5.96, 0, 27.71, 24),
-                width: double.infinity,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                     IconButton(
-                        icon: Icon(Icons.arrow_back),
-                        onPressed: () =>Navigator.pushReplacement(
+          Container(
+            margin: EdgeInsets.fromLTRB(5.96, 0, 27.71, 24),
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context),
+                  color: Warna.white,
+                ),
+                // Add space between the arrow icon and the favorite image
+                Spacer(),
+                Positioned(
+                  right: 4,
+                  top: 4,
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => ExploreScreen()),
-                      ),
-                        color: Warna.white,
-                      ),
-                    Container(
-                      width: 36,
-                      height: 36,
-                      child: Icon(
-                        isFavorite ? Icons.favorite : Icons.favorite_border,
-                        size: 34,
-                        color:  Color(0xff3fbcfc),
+                          MaterialPageRoute(
+                            builder: (context) => FavoritForm(book: widget.book),
+                          ),
+                        );
+                      },
+                      child: SizedBox(
+                        width: 32,
+                        height: 32,
+                        child: Image.asset(
+                          'assets/icons/Favorite.png',
+                          width: 36,
+                          height: 36,
+                        ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
+            ),
+          ),
+
 
 // <<< Details >>> //
 
@@ -311,7 +328,7 @@ class _DetailScreenState extends State<DetailScreen> {
 
               Container(
                 margin: EdgeInsets.fromLTRB(1, 0, 28, 29),
-                width: 350,
+                width: double.infinity,
                 child: TextButton(
                   onPressed: () {
                     Navigator.push(
@@ -326,7 +343,7 @@ class _DetailScreenState extends State<DetailScreen> {
                   ),
                   child: Container(
                     padding: EdgeInsets.fromLTRB(96.68, 12, 94.43, 12),
-                    width: 350,
+                    width: double.infinity,
                     height: 40,
                     decoration: BoxDecoration(
                       color: Color(0xff2a4cf1),
@@ -370,7 +387,7 @@ class _DetailScreenState extends State<DetailScreen> {
 
               Container(
                   margin:  EdgeInsets.fromLTRB(0, 0, 0, 8),
-                  width: 700,
+                  width: double.infinity,
                   child:  
                 const Text(
                   'Description Book',
@@ -386,7 +403,7 @@ class _DetailScreenState extends State<DetailScreen> {
               Container(
                 margin: EdgeInsets.fromLTRB(1, 0, 0, 32),
                 constraints: const BoxConstraints(
-                  maxWidth: 350,
+                  maxWidth: 400,
                 ),
                 child:  Text(
                   "${widget.book.fields.bookDesc}",
@@ -404,7 +421,7 @@ class _DetailScreenState extends State<DetailScreen> {
 
              Container(
               margin:  EdgeInsets.fromLTRB(0, 0, 15, 14.5),
-              width:  350,
+              width:  double.infinity,
               height:  28,
               child:  
                 Row(
@@ -514,7 +531,7 @@ class _DetailScreenState extends State<DetailScreen> {
 // <<< Similar Book >>> //
 Container(
   margin: EdgeInsets.fromLTRB(2, 0, 0, 0),
-  width: 350,
+  width: double.infinity,
   child: Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -549,121 +566,147 @@ Container(
                 itemCount: snapshot2.data!.length,
                 itemBuilder: (context, index) {
                   Book book = index < snapshot2.data!.length ? snapshot2.data![index] : Book(model: widget.book.model, pk: widget.book.pk,fields: widget.book.fields);
-                  return Container(
-                    padding: EdgeInsets.fromLTRB(8, 8, 8, 16),
-                    width: 152,
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Color(0xff292c4f),
-                      borderRadius: BorderRadius.circular(18.3132534027),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Color(0x05a3a3a3),
-                          offset: Offset(3.6626505852, 3.6626505852),
-                          blurRadius: 9.1566267014,
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailScreen(book: book),
                         ),
-                      ],
-                    ),
+                      );
+                    },
                     child: Container(
-                      width: double.infinity,
-                      height: double.infinity,
+                      margin: EdgeInsets.all(8.0),
+                      width: 152.0,
+                      decoration: BoxDecoration(
+                        color: Warna.backgroundlight,
+                        borderRadius: BorderRadius.circular(18.3132534027),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color(0x05a3a3a3),
+                            offset: Offset(3.6626505852, 3.6626505852),
+                            blurRadius: 9.1566267014,
+                          ),
+                        ],
+                      ),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                         Container(
-                            margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                          Container(
                             width: double.infinity,
-                            height: 164,
+                            height: 200,
                             decoration: BoxDecoration(
-                              color: Color(0xff22264f),
+                              color: Warna.backgroundlight,
                               borderRadius: BorderRadius.circular(9.99081707),
                             ),
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => DetailScreen(book: book),
-                                  ),
-                                );
-                              },
-                              child: Stack(
-                                children: [
-                                  Positioned(
-                                    left: -2.0607910156,
-                                    top: 0,
-                                    child: Align(
-                                      child: Container(
-                                        width: 128,
-                                        height: 190,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(12.0),
-                                          image: DecorationImage(
-                                            image: NetworkImage(book.fields.imageUrl),
-                                            fit: BoxFit.cover,
-                                          ),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  left: 0,
+                                  top: 0,
+                                  right: 0,
+                                  bottom: 0,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: SizedBox(
+                                      width: 137.91,
+                                      height: 185,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                        child: CachedNetworkImage(
+                                          imageUrl: "${book.fields.imageUrl}",
+                                          fit: BoxFit.cover,
                                         ),
                                       ),
                                     ),
                                   ),
-                                ],
-                              ),
+                                ),
+                                Positioned(
+                                  right: 4,
+                                  top: 4,
+                                  child: Align(
+                                    alignment: Alignment.topRight,
+                                    child: InkWell(
+                                      onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => FavoritForm(book: book),
+                                            ),
+                                          );
+                                      },
+                                      child: SizedBox(
+                                        width: 32,
+                                        height: 32,
+                                        child: Image.asset(
+                                          'assets/icons/Favorite.png',
+                                          width: 36,
+                                          height: 36,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-
                           Container(
+                            width: double.infinity,
+                            color: Warna.backgroundlight,
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Container(
-                              margin: EdgeInsets.fromLTRB(0, 0, 0, 4),
-                              constraints: const BoxConstraints(
-                                maxWidth: 135,
-                              ),
-                              child: Text(
-                                (book?.fields?.bookTitle?.length ?? 0) > 18
+                                  padding: EdgeInsets.symmetric(horizontal: 10),
+                                  margin: EdgeInsets.fromLTRB(0, 0, 0, 4),
+                                  constraints: const BoxConstraints(
+                                    maxWidth: 135,
+                                  ),
+                                  child: Text(
+                                    (book?.fields?.bookTitle?.length ?? 0) > 18
                                     ? (book?.fields?.bookTitle?.substring(0, 18) ?? '') + '...'
                                     : book?.fields?.bookTitle ?? '',
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w700,
-                                  height: 1.3333333333,
-                                  color: Color(0xffffffff),
-                                ),
-                              ),
-                            ),
-                            Text(
-                              'by ${
-                                (book?.fields?.bookAuthors?.length ?? 0) > 18
-                                    ? (book?.fields?.bookAuthors?.substring(0, 18)?.replaceAll("|", ", ") ?? '') + '...'
-                                    : book?.fields?.bookAuthors?.replaceAll("|", ", ") ?? ''
-                              }',
-                              style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                                height: 1.6,
-                                color: Color(0xffb6b6b6),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: 12 ,
-                                        ),
-                                      ],
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w700,
+                                      height: 1.3333333333,
+                                      color: Color(0xffffffff),
                                     ),
                                   ),
-                                );
-                              },
-                            );
-                          }
-                        },
+                                ),
+                                Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 10),
+                                  child: Text(
+                                    'by ${
+                                      (book?.fields?.bookAuthors?.length ?? 0) > 18
+                                          ? (book?.fields?.bookAuthors?.substring(0, 18)?.replaceAll("|", ", ") ?? '') + '...'
+                                          : book?.fields?.bookAuthors?.replaceAll("|", ", ") ?? ''
+                                    }',
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                      height: 1.6,
+                                      color: Color(0xffb6b6b6),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
+                  );
+                }
+              );
+            }
+          }
+        )
+            )
+              ],
+
                 ),
               ),
             ],
