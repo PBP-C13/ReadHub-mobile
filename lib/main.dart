75d25af4513f1700d458b24c7717d6bd743df1f3
syifakaffa <syifa.kaffa@ui.ac.id>
2023-12-19 11:45:56 +0700

    ubah main jadi splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
           useMaterial3: true,
         ),
-        home:ProfileScreen(),
+        home: SplashScreen(),
       ),
     );
   }
